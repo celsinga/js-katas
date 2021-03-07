6e01f1a88a58260ed27efb077b200f3a3eb42d54
Colin Elsinga <colinelsinga@Colins-MBP-2.router.taotronics.com>
2021-03-06 19:20:26 -0800

    arr to str

--- multiplication.js
+++ multiplication.js
@@ -1,14 +1,11 @@
 const multiplicationTable = function(maxValue) {
-  let arr = [];
+  let str = "";
   for (let i = 1; i <= maxValue; i++) {
-    arr.push(i);
-    for (let x = 0; x < arr.length; x++) {
-      arr[x] += ` ${i}`
-    }
+    str += ` ${i}`;
   }
-  return arr;
+  return str;
 };
 
-// console.log(multiplicationTable(1));
+console.log(multiplicationTable(1));
 console.log(multiplicationTable(5));
-// console.log(multiplicationTable(10));
+console.log(multiplicationTable(10));
